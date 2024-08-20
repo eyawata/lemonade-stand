7159900ba5067df3dc45f00abf7c258ad3b8c976
Jonas Al-Taher <jonas.taher@hotmail.com>
2024-08-20 10:34:04 +0900

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  resources :products, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :orders, only: [:edit, :show, :new, :create] do
+    resources :order_products, only: [:new, :create, :update, :edit, :destroy]
+  end
+  resources :events, only: [:edit, :show, :new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
