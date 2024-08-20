96f87bd10cc2e3a1c15c1822118bb9f127548cbd
Eri Yawata <yawata.eri@gmail.com>
2024-08-20 15:07:06 +0900

    initial commit for orders#show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,9 @@
 class OrdersController < ApplicationController
   def show
+    # find order by params id
+    # for each order_product, display it in a list
+    # aggregate the total and display end of the list
+
     @order = Order.find(params[:id])
   end
 
