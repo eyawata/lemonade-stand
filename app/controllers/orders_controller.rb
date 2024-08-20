15fe00f00bf621f165ae1e644bf47209d6101eaa
Eri Yawata <yawata.eri@gmail.com>
2024-08-20 18:58:14 +0900

    added onto order controller and view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
     # for each order_product, display it in a list
     # aggregate the total and display end of the list
     @order = Order.find(params[:id])
-    raise
+    @order_subtotal = subtotal
   end
 
   def index
@@ -13,5 +13,16 @@ class OrdersController < ApplicationController
 
   def new
     @order = Order.new
+  end
+
+  private
+
+  def subtotal
+    subtotal = 0
+    @order.order_products.each do |op|
+      product_total = op.product_price_at_sale * op.product_quantity
+      subtotal = subtotal + product_total
+    end
+    return subtotal
   end
 end
