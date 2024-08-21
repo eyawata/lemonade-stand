6c05ee9decb16461866a72b58cf05681ef284d30
Eri Yawata <yawata.eri@gmail.com>
2024-08-21 18:42:26 +0900

    add update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,4 +24,19 @@ class OrdersController < ApplicationController
 
     # redirect_to order_path(@orders.last) if @orders.last&.status == "incomplete"
   end
+
+  def update
+    @order = Order.find(params[:id])
+
+    # update_inventory is an instance method in order.rb
+    @order.update_inventory
+
+    # mark order as complete and order's total price
+    @order.update(status: "completed", total_price: @order.subtotal)
+
+    # redirect to orders/new
+    redirect_to new_order_path
+
+    # flash order "order confirmed"
+  end
 end
