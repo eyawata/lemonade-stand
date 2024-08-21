edd73c0b474b741ca3ab3b762c18ed103f44eb45
ali haddi <alij2297@hotmail.com>
2024-08-21 16:11:40 +0900

    Add:+ and - order products

--- app/javascript/controllers/quantity_controller.js
+++ app/javascript/controllers/quantity_controller.js
@@ -2,7 +2,36 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="quantity"
 export default class extends Controller {
+  static targets = ["productQuantity", "showProductQuantity"]
+
   connect() {
     console.log("Welcome back Stimulus!")
   }
+
+  add(event) {
+    event.preventDefault();
+    console.log("Add button clicked");
+
+    let quantity = parseInt(this.productQuantityTarget.innerText);
+    let orderProductQuantity = parseInt(this.showProductQuantityTarget.innerText);
+
+    if (orderProductQuantity < quantity) {
+      this.showProductQuantityTarget.innerText = orderProductQuantity + 1;
+    } else {
+      console.log("No Item to add");
+      }
+  }
+
+  subtract(event) {
+    event.preventDefault();
+    console.log("Subtract button clicked");
+
+     let orderProductQuantity = parseInt(this.showProductQuantityTarget.innerText);
+
+    if (orderProductQuantity > 0) {
+      this.showProductQuantityTarget.innerText = orderProductQuantity - 1;
+      } else {
+      console.log("No Item to subtract");
+      }
+    }
 }
