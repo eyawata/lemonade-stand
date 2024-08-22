e903105d84ae853538e48c590cf5a77110958314
Jonas Al-Taher <jonas.taher@hotmail.com>
2024-08-22 10:22:08 +0900

    Stimulus controller created for edit

--- app/javascript/controllers/product_modal_controller.js
+++ app/javascript/controllers/product_modal_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
   connect() {
     console.log("Hello")
   }
-  // fetchProductData(productId){
-  //   fetch
+  fetchProductData(productId){
+    fetch
   }
 }
