3359e2b4bd50966b5965d05f3d76cc5815299ef1
Jonas Al-Taher <jonas.taher@hotmail.com>
2024-08-23 15:20:43 +0900

    finished PWA configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,3 +76,4 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 end
+config.hosts.clear
