5f95bc2adb09332ddec4f4df8427a6200220bdb3
Jonas Al-Taher <jonas.taher@hotmail.com>
2024-08-26 15:31:36 +0900

    events index form draft created

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,7 +13,14 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(event_params)
+    @event.user = current_user
     assign
+    if @event.save
+      redirect_to events_path, notice: "Event was susuccessfully created"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
@@ -22,5 +29,9 @@ class EventsController < ApplicationController
     # todo
     # iterate through the events
     # calls order instance method
+  end
+
+  def event_params
+    params.require(:event).permit(:start_date, :end_date, :estimated_event_cost, :event_name, :photo)
   end
 end
