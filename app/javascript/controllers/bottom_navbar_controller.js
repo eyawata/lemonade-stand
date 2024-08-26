27ab1ecde41b1c802cd89f160328f8cde0c1632c
sadanan vorlaruxtara <sadanan.vorlaruxtara@gmail.com>
2024-08-26 16:40:42 +0900

    Added Bottom Navbar but have not fixed other parts

---
+++ app/javascript/controllers/bottom_navbar_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="bottom-navbar"
+export default class extends Controller {
+  static targets = ['links', 'circle' ]
+  static values = { value: Number }
+
+  connect() {
+    console.log("connected!");
+    this.linksTargets.forEach(link => {
+      link.addEventListener('click', this.moveCircle.bind(this));
+    });
+  }
+
+  moveCircle(event) {
+    const link = event.currentTarget;
+    const value = link.getAttribute('data-bottom-navbar-value');
+
+    this.linksTargets.forEach(link => {
+      link.style.opacity = 1;
+    });
+
+    this.circleTarget.style.left = `${21 * value}vw`;
+    this.circleTarget.innerHTML = link.innerHTML;
+    link.style.opacity = 0;
+  }
+
+}
