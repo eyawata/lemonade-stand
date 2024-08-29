5e7e1cfd442f63e4d4d31f14d5b53e844ea59a4d
sadanan vorlaruxtara <sadanan.vorlaruxtara@gmail.com>
2024-08-29 16:24:10 +0900

    fixed an error for csv upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,20 +16,15 @@ class User < ApplicationRecord
 
   def my_import(file)
     products = []
-    if params[:file].blank?
-      flash[:notice] = "No file attached"
-      redirect_to products_path
-    else
-      CSV.foreach(file.path, headers: true, header_converters: :symbol) do |row|
-        product = Product.new(name: row[:name], price: row[:price], category: row[:category], quantity: row[:quantity])
-        product.user = self
-        product.valid?
-        product.save
+    CSV.foreach(file.path, headers: true, header_converters: :symbol) do |row|
+      product = Product.new(name: row[:name], price: row[:price], category: row[:category], quantity: row[:quantity])
+      product.user = self
+      product.valid?
+      product.save
 
-        file = URI.open(row[:image_url])
-        product.photo.attach(io: file, filename: "#{product.name}.jpg", content_type: 'image/jpg')
-        products << product
-      end
+      file = URI.open(row[:image_url])
+      product.photo.attach(io: file, filename: "#{product.name}.jpg", content_type: 'image/jpg')
+      products << product
     end
   end
 end
