af958e84dcc9ce3b4ed984ce57809b2163b6444f
Eri Yawata <yawata.eri@gmail.com>
2024-08-29 19:37:00 +0900

    edit link

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Event.destroy_all
 
 urls = {
   "Spotty Cup" => "https://res.cloudinary.com/djqladxhq/image/upload/v1724887832/production/37xd7p8imi78sjsz9ybvbo59gz6l.jpg",
-  "Northern Lights Planter" => "https://res.cloudinary.com/djqladxhq/image/upload/v1724927167/development/fcw1538q2q03vui7fqi168aqc34b.jpg",
+  "Northern Lights Planter" => "https://res.cloudinary.com/djqladxhq/image/upload/v1724887832/production/37xd7p8imi78sjsz9ybvbo59gz6l.jpg",
   "Aurora Cup" => "https://res.cloudinary.com/djqladxhq/image/upload/v1724888012/production/0gn2oro44tus75la3q7lrp5bw5n8.jpg"
 }
 
